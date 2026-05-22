bac3a3a41ac82f189e92ed28c67b21429e573b03
v28650-stack <v28650@gmail.com>
2026-05-22 11:41:57 -0400

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,6 +1,8 @@
-OLED_ENABLE = yes
-WPM_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
 VIA_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+WPM_ENABLE = yes
+OLED_ENABLE = yes
 LTO_ENABLE = yes
+
 SRC += ocean_dream.c
+SRC += oled_graphics.c
