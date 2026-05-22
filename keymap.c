86f5f57811d67365e752d00284ce8c47f57720b7
v28650-stack <v28650@gmail.com>
2026-05-22 11:41:38 -0400

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,8 +1,6 @@
-// Copyright 2024 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 #include "ocean_dream.h"
+#include "oled_graphics.h"
 
 enum sofle_layers {
     _QWERTY,
@@ -13,9 +11,6 @@ enum sofle_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/*
- * QWERTY
- */
 [_QWERTY] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
@@ -24,9 +19,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       KC_LGUI, KC_LALT, KC_LCTL, MO(_LOWER), KC_ENT, KC_SPC, MO(_RAISE), KC_RCTL, KC_RALT, KC_RGUI
 ),
 
-/*
- * LOWER
- */
 [_LOWER] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
@@ -35,9 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______
 ),
 
-/*
- * RAISE
- */
 [_RAISE] = LAYOUT(
     _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
     _______, KC_INS,  KC_PSCR, KC_APP,  XXXXXXX, XXXXXXX,                    KC_PGUP, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, KC_BSPC,
@@ -46,9 +35,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______
 ),
 
-/*
- * ADJUST
- */
 [_ADJUST] = LAYOUT(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -56,27 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
                       _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______
 )
-
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_QWERTY] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(KC_PGDN, KC_PGUP)
-    },
-    [_LOWER] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(KC_PGDN, KC_PGUP)
-    },
-    [_RAISE] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(KC_PGDN, KC_PGUP)
-    },
-    [_ADJUST] = {
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(KC_PGDN, KC_PGUP)
-    }
+    [_QWERTY] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_LOWER]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_RAISE]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
 
@@ -87,29 +60,20 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        oled_write_P(PSTR("Layer"), false);
 
-        switch (get_highest_layer(layer_state)) {
-            case _QWERTY:
-                oled_write_P(PSTR("BASE "), false);
-                break;
-            case _LOWER:
-                oled_write_P(PSTR("LOWER"), false);
-                break;
-            case _RAISE:
-                oled_write_P(PSTR("RAISE"), false);
-                break;
-            case _ADJUST:
-                oled_write_P(PSTR("ADJST"), false);
-                break;
-            default:
-                oled_write_P(PSTR("?????"), false);
-                break;
-        }
-    } else {
+    // Slave = animation
+    if (!is_keyboard_master()) {
         render_ocean_dream();
+        return false;
     }
+
+    // Master = stats
+    oled_clear();
+
+    render_logo();
+    render_wpm();
+    render_caps();
+    render_layer_icon(get_highest_layer(layer_state));
 
     return false;
 }
@@ -123,6 +87,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             is_calm = record->event.pressed;
             break;
     }
-
     return true;
 }
