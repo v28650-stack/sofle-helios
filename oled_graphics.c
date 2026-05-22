cae6de2373e189ac949fec8b29fa0c6d639e960c
v28650-stack <v28650@gmail.com>
2026-05-22 14:12:51 -0400

    Update oled_graphics.c

--- oled_graphics.c
+++ oled_graphics.c
@@ -1,12 +1,12 @@
 #include "quantum.h"
 
-void oled_task_user(void) {
-    // show different text on left vs right half if split
+bool oled_task_user(void) {
     if (is_keyboard_master()) {
-        oled_set_cursor(0,0);
+        oled_set_cursor(0, 0);
         oled_write_ln("LEFT OLED OK", false);
     } else {
-        oled_set_cursor(0,0);
+        oled_set_cursor(0, 0);
         oled_write_ln("RIGHT OLED OK", false);
     }
+    return true;
 }
